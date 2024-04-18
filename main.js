0ffc9eca30b9ba926aff06ebde367ef94cb4ef01
felix <felix.courtin@gmail.com>
2024-04-18 14:35:46 +0200

    Fonction écriture dans un fichier

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import * as R from 'ramda'
+import { writeFileSync } from 'fs';
 
 const predjson = 'fonction créant fichier json contenant les resultats des preds';
 
 console.log(predjson);
+
+
+const data = 'Contenu à écrire dans le fichier';
+
+try {
+    writeFileSync('results.txt', data);
+    console.log('Le fichier a été écrit avec succès.');
+} catch (error) {
+    console.error('Une erreur s\'est produite lors de l\'écriture du fichier :', error);
+}
