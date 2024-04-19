dceaef40d7cc55f1170e589310e098591d516efa
felix <felix.courtin@gmail.com>
2024-04-19 18:01:04 +0200

    des trucs qui marchent pas

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as R from 'ramda';
+import * as fs from 'fs';
 import {writeFileSync} from 'fs';
 import csv from 'csv-parser';
-import * as fs from "fs";
 
 const data = '';
 
@@ -19,25 +19,14 @@ createfile(data);
 
 
 let filteredRows = [];
-const filterRowsByMedals = row => R.pipe(
-    R.prop('Medal'),
-    R.equals(R.__, "Gold")
-)(row);
+const filterRowsByMedals = row => R.pipe(R.prop('Medal'), R.equals(R.__, "Gold"))(row);
 
-const filterRowsByEdition = row => R.pipe(
-    R.prop('Year'),
-    parseInt,
-    R.equals(R.__, 2016)
-)(row);
-const filterRowsByAge = row => R.pipe(
-    R.prop('Age'),
-    parseInt,
-    R.lt(R.__, 25)
-)(row);
+const filterRowsByEdition = row => R.pipe(R.prop('Year'), parseInt, R.equals(R.__, 2016))(row);
+const filterRowsByAge = row => R.pipe(R.prop('Age'), parseInt, R.lt(R.__, 25))(row);
 fs.createReadStream('athlete_events.csv')
     .pipe(csv())
     .on('data', (row) => {
-        // Si la ligne passe le filtre, l'ajouter au tableau
+
         if (filterRowsByMedals(row)) {
             if (filterRowsByAge(row)) {
                 if (filterRowsByEdition(row)) {
@@ -47,8 +36,8 @@ fs.createReadStream('athlete_events.csv')
         }
     })
     .on('end', () => {
-        // Insérer '[' avant la première ligne et ']' après la dernière ligne
-        const filteredData = `[${filteredRows.map(row => JSON.stringify(row)).join(',\n')}]`;
+
+        const filteredData = `[\n${filteredRows.map(row => '\t' + JSON.stringify(row)).join(',\n')}\n]`;
         fs.writeFile('results.json', filteredData, (err) => {
             if (err) {
                 console.error('Erreur lors de l\'écriture du fichier :', err);
@@ -59,28 +48,11 @@ fs.createReadStream('athlete_events.csv')
     });
 
 
-
 const countIds = jsonData => {
-    const idCounts = {};
-    jsonData.forEach(item => {
-        const id = item.id;
-        idCounts[id] = (idCounts[id] || 0) + 1;
-    });
-    return idCounts;
-}
+    console.log(R.pipe(R.map(R.prop('ID')), R.countBy(R.identity))(jsonData));
+};
 
-// Lire le fichier JSON
-fs.readFile('results.json', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Erreur lors de la lecture du fichier :', err);
-        return;
-    }
+countIds('results.json');
 
-    try {
-        const jsonData = JSON.parse(data);
-        const idCounts = countIds(jsonData);
-        console.log('Occurrences des IDs :', idCounts);
-    } catch (error) {
-        console.error('Erreur lors de l\'analyse du fichier JSON :', error);
-    }
-});
+
+
