2e75ef96a9e40dd2ab9ebff1d39cbf5038a0d8fc
felix <felix.courtin@gmail.com>
2024-04-19 15:15:34 +0200

    3 filtres

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Félix Courtin
 Theo Lucq
 
 Ce projet vise à prédire des résultats aux différentes épreuves des Jeux Olympiques en fonction des précédentes éditions.
-
+ 
--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-import * as R from 'ramda'
+import * as R from 'ramda';
 import {writeFileSync} from 'fs';
 import csv from 'csv-parser';
 import * as fs from "fs";
-import {table} from "@observablehq/inputs";
 
 const data = 'trucs à écrire dans le fichier';
 
 const createfile = (text) => {
     try {
         writeFileSync('results.txt', text);
-        console.log('Le fichier a été écrit avec succès.');
+        console.log('Le fichier a été créé avec succès.');
     } catch (error) {
         console.error('Une erreur s\'est produite lors de l\'écriture du fichier :', error);
     }
@@ -20,17 +19,31 @@ createfile(data);
 
 
 let filteredRows = [];
-const filterRows = row => R.pipe(
+const filterRowsByMedals = row => R.pipe(
     R.prop('Medal'),
-    //parseInt,
     R.equals(R.__, "Gold")
+)(row);
+
+const filterRowsByEdition = row => R.pipe(
+    R.prop('Year'),
+    parseInt,
+    R.equals(R.__, 2016)
+)(row);
+const filterRowsByAge = row => R.pipe(
+    R.prop('Age'),
+    parseInt,
+    R.lt(R.__, 25)
 )(row);
 fs.createReadStream('athlete_events.csv')
     .pipe(csv())
     .on('data', (row) => {
         // Si la ligne passe le filtre, l'ajouter au tableau
-        if (filterRows(row)) {
-            filteredRows.push(row);
+        if (filterRowsByMedals(row)) {
+            if (filterRowsByAge(row)) {
+                if (filterRowsByEdition(row)) {
+                    filteredRows.push(row);
+                }
+            }
         }
     })
     .on('end', () => {
@@ -40,6 +53,6 @@ fs.createReadStream('athlete_events.csv')
                 console.error('Erreur lors de l\'écriture du fichier :', err);
                 return;
             }
-            console.log('Les lignes filtrées ont été écrites dans results.txt');
+            console.log('Les lignes filtrées ont été écrites dans results.txt.');
         });
     });
